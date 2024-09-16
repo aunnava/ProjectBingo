706826f92c112078c365a224ce753b0c5b0cb071
Anurag Unnava <anuragunnava@gmail.com>
2024-09-16 05:32:39 +0200

    Simple WebAR - click to move ant

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,19 +6,54 @@ using UnityEngine.AI;
 public class Movement : MonoBehaviour
 {
 
-   [SerializeField] private Camera mainCamera;
     private RaycastHit hit;
     private NavMeshAgent agent;
+    public bool useClickToMove;
+    private Rigidbody agentRigidBody;
 
-
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private VariableJoystick joystick;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private GameObject MoveToPointMarker;
+    
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        if(!useClickToMove)
+        agentRigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+       
+
+    }
+
+    private void FixedUpdate()
+    {
+        if(!useClickToMove)
+        MoveWithJoystick();
+        if (useClickToMove)
+        {
+            ClickToMove();
+        }
+    }
+
+    private void MoveWithJoystick()
+    {
+        agentRigidBody.velocity = new Vector3(joystick.Horizontal * moveSpeed, agentRigidBody.velocity.y, joystick.Vertical * moveSpeed);
+
+        if (joystick.Horizontal != 0 || joystick.Vertical != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(agentRigidBody.velocity);
+        
+        }
+
+    }
+
+    private void ClickToMove()
     {
         if (Input.GetMouseButtonDown(0))
         {
@@ -27,9 +63,12 @@ public class Movement : MonoBehaviour
                 if (hit.collider.CompareTag("Ground"))
                 {
                     agent.SetDestination(hit.point);
+                    MoveToPointMarker.transform.position= hit.point;
                 }
 
             }
         }
     }
+
+
 }
